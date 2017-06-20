de2f260fed444456c0e1c3d559d78d9b90aff7c6
amullis <amullis@umich.edu>
2017-06-20 10:50:31 -0600

    Improving cosine wave and adding noise

--- R/Alicia_SeasonalSIRS.R
+++ R/Alicia_SeasonalSIRS.R
@@ -7,7 +7,7 @@ seasonal_sir <- function (t, x, parms) {
   with(
     as.list(c(x, parms)),
     {
-      beta <- beta0 * (1 + beta1*cos(6*t+0.05))     
+      beta <- beta0 * (1 + beta1*(cos(6*t+0.05)-0.6))     
       dS <- mu * (1-S) - beta*S*I+d*R                
       dI <- beta * S * I - (mu+gamma)*I
       dR <- gamma * I - mu*R-d*R
@@ -17,8 +17,8 @@ seasonal_sir <- function (t, x, parms) {
   )
 }
 
-times <- seq(0,20,by=1/365)                     
-params <- c(mu=0,beta0=6*365,beta1=.8,gamma=365/13, d=365/180)   #parameters 
+times <- seq(0,100,by=1/365)                     
+params <- c(mu=0,beta0=1*365,beta1=.5,gamma=365/13, d=365/180)   #parameters 
 xstart <- c(S=0.09,I=0.001,R=0.899)              #initial conditions
 out <- as.data.frame(lsoda(y = xstart,
                            times = times,
@@ -28,6 +28,13 @@ out <- as.data.frame(lsoda(y = xstart,
 out %>% gather(key, value, S:R) %>%
   ggplot(aes(time, value, color = key)) + geom_line()
 
+plot(out$I[1:3650], type='l')
+seasonal<-out$I
+seasonal.noise<-rnorm(length(seasonal), seasonal, sd=sd(seasonal))
+
+par(mfrow=c(1,2))
+plot(seasonal[1:3650], type='l')
+plot(seasonal.noise[1:3650], type='l')
 
 #curve(5/365 * (1 + .8*cos(6*x)), from=0, to=2)
 
